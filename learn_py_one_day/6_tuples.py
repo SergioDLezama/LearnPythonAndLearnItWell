2073813fb0f27c78b1d054e49711066c975c1499
Sergio Lezama <sergiodavidlezama@gmail.com>
2023-10-12 19:49:16 -0400

    End Chapter 4

---
+++ learn_py_one_day/6_tuples.py
@@ -0,0 +1,18 @@
+'''
+Tuples
+
+They are just like lists, but you cannot modify their values
+
+Syntax:
+tupleName = (initial values separated by commas)
+'''
+
+monthsOfYear = (
+    'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+    )
+
+print(monthsOfYear) # ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec')
+print(monthsOfYear[0]) # Jan
+print(monthsOfYear[2:6]) # ('Mar', 'Apr', 'May', 'Jun')
+
+
